26bb4b230b8589737dc5c9d0ddd37100f3f6b4c3
Ege Iper <ege.iper@launchmetrics.com>
2023-01-01 16:08:00 +0300

    version 1.0.6

--- src/main/java/org/egeiper/PropertyUtils.java
+++ src/main/java/org/egeiper/PropertyUtils.java
@@ -1,14 +1,13 @@
 package org.egeiper;
 
 import java.io.*;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 public final class PropertyUtils {
     private static final String EXCEPTION_MESSAGE = "Invalid property key or resource file";
-    private static final String RESOURCE_DIRECTORY = "/Users/egeiper/IdeaProjects/QA-Base/src/test/resources";
+    private static final String RESOURCE_DIRECTORY = "/Users/egeiper/IdeaProjects/QA-Base/src/main/resources/";
     private PropertyUtils() {
     }
 
@@ -16,8 +15,7 @@ public final class PropertyUtils {
     public static String getProperty(final String fileName, final String propertyKey) {
         final Properties prop = new Properties();
         try (BufferedReader reader =
-                     Files.newBufferedReader(Paths.get(URI.create(
-                             String.format("%s/%s", RESOURCE_DIRECTORY, fileName))))) {
+                     Files.newBufferedReader(Paths.get(RESOURCE_DIRECTORY + fileName))) {
             prop.load(reader);
         } catch (IOException e) {
             throw new FileOrResourceNotFoundException(EXCEPTION_MESSAGE, e);
